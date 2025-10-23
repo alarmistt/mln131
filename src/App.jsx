977859b6602ecc54bc2286acc4abd880dc2256c9
Minh Đức <139892190+duckonzz@users.noreply.github.com>
2025-10-23 16:55:06 +0700

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Homepage from "./component/Homepage";
 import "./App.css";
 
